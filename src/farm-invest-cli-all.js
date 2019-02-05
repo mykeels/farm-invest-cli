2c1631e638471ff890ef0428d27d430ada30bb41
Ikechi Michael <mykehell123@gmail.com>
2019-02-05 09:43:01 +0100

    feat: add support for agropartnerships.co

--- src/farm-invest-cli-all.js
+++ src/farm-invest-cli-all.js
@@ -1,3 +1,4 @@
+const getAgro = require('./sites/agro')
 const getEFarms = require('./sites/efarms')
 const getFarmCrowdy = require('./sites/farm-crowdy')
 const getThriveAgric = require('./sites/thrive-agric')
@@ -5,11 +6,13 @@ const fs = require('fs')
 
 const syncAll = async () => {
     try {
+        const syncAgro = require('./farm-invest-cli-agro')
         const syncEFarms = require('./farm-invest-cli-efarms')
         const syncFarmCrowdy = require('./farm-invest-cli-farm-crowdy')
         const syncThriveAgric = require('./farm-invest-cli-thrive-agric')
 
         return await Promise.all([
+            syncAgro({ getAgro, fs }),
             syncEFarms({ getEFarms, fs }),
             syncFarmCrowdy({ getFarmCrowdy, fs }),
             syncThriveAgric({ getThriveAgric, fs })
--- src/farm-invest-cli.js
+++ src/farm-invest-cli.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
+const getAgro = require('./sites/agro')
 const getEFarms = require('./sites/efarms')
 const getFarmCrowdy = require('./sites/farm-crowdy')
 const getThriveAgric = require('./sites/thrive-agric')
 const syncAll = require('./farm-invest-cli-all')
+const syncAgro = require('./farm-invest-cli-agro')
 const syncEFarms = require('./farm-invest-cli-efarms')
 const syncFarmCrowdy = require('./farm-invest-cli-farm-crowdy')
 const syncThriveAgric = require('./farm-invest-cli-thrive-agric')
@@ -14,6 +16,7 @@ if (require.main === module) {
 
     program
         .version(pjson.version)
+        .command('agro [env]', 'shows latest info on agro partnerships')
         .command('efarms [env]', 'shows latest info on efarms')
         .command('farm-crowdy [env]', 'shows latest info on farm-crowdy')
         .command('thrive-agric [env]', 'shows latest info on thrive-agric')
@@ -21,10 +24,12 @@ if (require.main === module) {
         .parse(process.argv)
 }
 
+module.exports.getAgro = getAgro
 module.exports.getEFarms = getEFarms
 module.exports.getFarmCrowdy = getFarmCrowdy
 module.exports.getThriveAgric = getThriveAgric
 module.exports.syncAll = syncAll
+module.exports.syncAgro = syncAgro
 module.exports.syncEFarms = syncEFarms
 module.exports.syncFarmCrowdy = syncFarmCrowdy
 module.exports.syncThriveAgric = syncThriveAgric
