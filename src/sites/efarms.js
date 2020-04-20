e5034085f4f0d407821cdfd4989281750d4b54aa
lilpolymath <ayofavour7@gmail.com>
2020-04-20 01:41:17 +0100

    feat: Update scrapping function for EFarms.

--- src/sites/efarms.js
+++ src/sites/efarms.js
@@ -1,26 +1,46 @@
-const { default : axios } = require('axios')
-const cheerio = require('cheerio')
-const fs = require('fs')
+const { default: axios } = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
 
-const { eFarmsHtml } = require('../utils/create-files-dir')
+const { eFarmsHtml } = require('../utils/create-files-dir');
 
-module.exports = function () {
-    return axios.get('https://www.efarms.com.ng/index.php/en/farms').then(res => {
-        const $ = cheerio.load(res.data)
+module.exports = function() {
+  return axios.get('https://www.efarms.com.ng/index.php/en/farms').then(res => {
+    const $ = cheerio.load(res.data);
 
-        fs.writeFileSync(eFarmsHtml, res.data)
-        
-        const activeProducts = $('section.causes div.grid')
+    fs.writeFileSync(eFarmsHtml, res.data);
 
-        const productList = activeProducts.map(function () {
-            const title = $(this).find('h3 a').text().trim()
-            const price = $(this).find('div.raised').text().trim();
-            const returns = $(this).find('div.goal').text().trim();
-            const daysLeft = $(this).find('span.remaining-days').text().trim();
-            const link = $(this).find('h3 a').attr('href').trim()
-            return { title, price, link, returns, daysLeft, farm: 'efarms' }
-        }).toArray().filter(({ daysLeft }) => daysLeft && !daysLeft.endsWith('ago'))
+    const activeProducts = $('section.causes div.grid');
 
-        return productList
-    })
-}
+    const productList = activeProducts
+      .map(function() {
+        const title = $(this)
+          .find('h3 a')
+          .text()
+          .trim();
+        const price = $(this)
+          .find('div.raised > h4 > span')
+          .text()
+          .trim();
+        const returns = $(this)
+          .find('div.goal > h4 > span')
+          .text()
+          .trim();
+        const daysLeft = $(this)
+          .find('span.remaining-days')
+          .text()
+          .trim();
+        const link = $(this)
+          .find('h3 a')
+          .attr('href')
+          .trim();
+        return { title, price, link, returns, daysLeft, farm: 'efarms' };
+      })
+      .toArray()
+      .filter(({ daysLeft }) => daysLeft && !daysLeft.endsWith('ago'));
+
+    console.table(productList);
+
+    return productList;
+  });
+};
